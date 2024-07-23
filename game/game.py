5970174be3e721133d82607ca2fd516a09ec3222
NahueAdema <ademanahuel@gmail.com>
2024-07-22 21:43:14 -0300

    Se agrego el final de juego y futuro boton  de reset game

--- game/game.py
+++ game/game.py
@@ -14,14 +14,15 @@ class Case:
 class DealOrNoDeal:
     def __init__(self, case_vals=CASE_VALS):
         self.case_vals = sorted(case_vals)
-        self.vals_left = case_vals.copy()  
+        self.vals_left = case_vals.copy()
         random.shuffle(case_vals)
         self.cases = [Case(value, num) for num, value in enumerate(case_vals, 1)]
         self.chosen_case = None
-        self.rounds = [5, 5, 5, 5, 3, 1]
+        self.rounds = [5, 5, 5, 5, 3, 1, 1, 1]
         self.current_round = 0
         self.offers = []
         self.playing = True
+        self.revealed_cases = set()  
 
     def get_cases(self):
         return [{"num": case.num, "disabled": not case.available, "selected": case == self.chosen_case} for case in self.cases]
@@ -36,13 +37,21 @@ class DealOrNoDeal:
     def reveal_cases(self, nums):
         revealed = []
         for num in nums:
+            if num in self.revealed_cases:
+                continue 
             if self.chosen_case and num == self.chosen_case.num:
-                continue  
+                continue 
             case = self.cases[num - 1]
             if case.value in self.vals_left:
                 case.available = False
                 self.vals_left.remove(case.value)
                 revealed.append((case.num, case.value))
+                self.revealed_cases.add(num) 
+        
+ 
+        if len(self.vals_left) <= 1:
+            self.playing = False 
+
         self.current_round += 1
         return revealed
 
@@ -62,6 +71,19 @@ class DealOrNoDeal:
         return self.offers
 
     def get_num_cases_to_reveal(self):
+        if not self.playing:
+            return 0 
         if self.current_round < len(self.rounds):
             return self.rounds[self.current_round]
         return 1
+
+    def check_game_end(self):
+        if len(self.vals_left) <= 1:
+            self.playing = False
+            return True
+        return False
+
+    def get_final_case_value(self):
+        if self.chosen_case:
+            return self.chosen_case.value
+        return None
--- game/routes.py
+++ game/routes.py
@@ -25,6 +25,8 @@ def reveal_cases():
     if request.method == 'POST':
         nums = [int(n) for n in request.form.getlist('case_nums')]
         revealed = game_instance.reveal_cases(nums)
+        if not game_instance.playing:
+            return redirect(url_for('game_bp.final')) 
         return render_template('reveal.html', revealed=revealed)
     else:
         num_to_reveal = game_instance.get_num_cases_to_reveal()
@@ -41,27 +43,32 @@ def get_offer():
 @game_bp.route('/accept_offer', methods=['POST'])
 @login_required
 def accept_offer():
-    case_num = game_instance.chosen_case.num
-    case_value = game_instance.chosen_case.value
-    offers = game_instance.get_offers()
-    return redirect(url_for('game_bp.final', case_num=case_num, case_value=case_value, offers=offers))
+    offer = game_instance.get_dealer_offer()
+    game_instance.playing = False  # Termina el juego
+    return redirect(url_for('game_bp.final', offer=offer))
 
 @game_bp.route('/reject_offer', methods=['POST'])
 @login_required
 def reject_offer():
-    return redirect(url_for('game_bp.next_round'))
-
-@game_bp.route('/next_round', methods=['GET', 'POST'])
-@login_required
-def next_round():
+    if game_instance.check_game_end():
+        return redirect(url_for('game_bp.final'))
+    if game_instance.current_round >= len(game_instance.rounds):
+        return redirect(url_for('game_bp.final'))
     game_instance.current_round += 1
     return redirect(url_for('game_bp.reveal_cases'))
-
 
 @game_bp.route('/final', methods=['GET'])
 @login_required
 def final():
-    case_num = request.args.get('case_num')
-    case_value = request.args.get('case_value')
+    case_num = game_instance.chosen_case.num if game_instance.chosen_case else None
+    case_value = game_instance.get_final_case_value() if game_instance.chosen_case else None
     offers = game_instance.get_offers()
-    return render_template('final.html', case_num=case_num, case_value=case_value, offers=offers)
+    offer = request.args.get('offer')  # Obtiene la oferta de la cadena de consulta
+    return render_template('final.html', case_num=case_num, case_value=case_value, offers=offers, offer=offer)
+
+@game_bp.route('/reset_game', methods=['POST'])
+@login_required
+def reset_game():
+    global game_instance
+    game_instance = GameInstance()  # Reinicia la instancia del juego
+    return redirect(url_for('game_bp.game_index'))
